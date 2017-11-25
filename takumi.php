e6e23b62d1996814d740089b9ee2a5b7c30c87e2
takumi <takumi@tk2-233-26149.vs.sakura.ne.jp>
2017-11-25 13:06:47 +0900

    takumi dayo-

---
+++ takumi.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>冬期岡本班</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no">
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+	<style type="text/css">
+		#fukidashi {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		#iamge {
+			width: 100%;
+		}
+	</style>
+</head>
+<body>
+<div id="wrapper">
+		<div id="fukidashi">......。</div>
+
+		<div id="image"">えるも画像</div>
+
+		<div id="story"></div>
+</div>
+
+</body>
+<script type="text/javascript">
+	$(function(){
+
+	}
+</script>
+</html>
