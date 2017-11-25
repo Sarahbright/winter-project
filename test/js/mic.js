070c5ac40e1ca318cddaca1459261b60b18efd0e
Ryota Suwa <r.s.30402457.m.p@gmail.com>
2017-11-25 16:48:24 +0900

    Fix mic.js

--- test/js/mic.js
+++ test/js/mic.js
@@ -4,9 +4,10 @@ function vr_function() {
     window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
     var recognition = new webkitSpeechRecognition();
     var speechRecognitionList = new webkitSpeechGrammarList();
-    var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+    // var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+    var colors = ['赤','黄','青','緑','白','黒'];
     var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
-    recognition.lang = 'en-US';
+    recognition.lang = 'ja-JP';
     recognition.interimResults = true;
     recognition.continuous = true;
     speechRecognitionList.addFromString(grammar, 1);
@@ -38,7 +39,7 @@ function vr_function() {
             document.getElementById('result_text').innerHTML = "一致しました";
           }
         });
-        vr_function();
+        vr_function()
         // for (var i = event.resultIndex; i < results.length; i++) {
         //     if (results[i].isFinal)
         //     {
